64bea2e93631e31b32478acfc6b1fae548b44d50
NipulK <168514285+NipulK@users.noreply.github.com>
2025-11-07 16:50:19 +0530

    load the images to board

--- pacman.js
+++ pacman.js
@@ -8,6 +8,17 @@ const boardwidth = colcount * cellsize;
 const boardheight = rowcount * cellsize;
 let context;
 
+//images
+let darkblue;
+let lightblue;
+let up;
+let down;
+let left;
+let right;
+let wall;
+
+
+
 //draw board
 window.onload = function() {
     board = document.getElementById("board"); // get board from HTML
@@ -15,5 +26,31 @@ window.onload = function() {
     board.height = boardheight;
     context = board.getContext("2d"); // used for drawing on the board
 
-    drawBoard();
+    
+    loadImages();
+}
+
+//load images
+function loadImages() {
+
+    //packman images
+    up = new Image();
+    up.src = "icons/up.png";
+    down = new Image();
+    down.src = "icons/down.png";
+    left = new Image();
+    left.src = "icons/left.png";
+    right = new Image();
+    right.src = "icons/right.png";
+
+    //wall image
+    wall = new Image();
+    wall.src = "icons/wall.png";
+
+    //board images
+    darkblue = new Image();
+    darkblue.src = "icons/darkblue.png";
+    lightblue = new Image();
+    lightblue.src = "icons/lightblue.png";
+
 }
