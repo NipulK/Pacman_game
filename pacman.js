ed3128e1497871d64f8b7738792fa11a91fe1daf
NipulK <168514285+NipulK@users.noreply.github.com>
2025-11-08 10:10:57 +0530

    make a map and make the map paths

--- pacman.js
+++ pacman.js
@@ -9,13 +9,39 @@ const boardheight = rowcount * cellsize;
 let context;
 
 //images
-let darkblue;
-let lightblue;
+let blueghost;
+let redghost;
+let yellowghost;
+let greenghost;
 let up;
 let down;
 let left;
 let right;
 let wall;
+
+
+//board setup 
+const tileMap = [
+  "XXXXXXXXXXXXXXXXXXX",
+  "X                 X",
+  "X XX XXX XXX XX XX",
+  "X X   X X X   X  X",
+  "XXXX XXXXX XXXXXXX",
+  "OOOXX X       XOOO",
+  "XXXX X XXrXX X XXXX",
+  "O      bpo      O",
+  "XXXX X XXXXX X XXXX",
+  "OOOX X     X XOOOX",
+  "XXXX X XXXXX X XXXX",
+  "X X   X     X   X X",
+  "X XX XXXXXXX XX XX",
+  "X   X   P   X    X",
+  "X X X XXXXX X X  X",
+  "X X X       X X  X",
+  "X XXXXXXXXXXXXX  X",
+  "X                X",
+  "XXXXXXXXXXXXXXXXXXX"
+];
 
 
 
@@ -47,10 +73,14 @@ function loadImages() {
     wall = new Image();
     wall.src = "icons/wall.png";
 
-    //board images
-    darkblue = new Image();
-    darkblue.src = "icons/darkblue.png";
-    lightblue = new Image();
-    lightblue.src = "icons/lightblue.png";
+    //ghost images
+    blueghost = new Image();
+    blueghost.src = "icons/blue.png";
+    redghost = new Image();
+    redghost.src = "icons/red.png";
+    yellowghost = new Image();
+    yellowghost.src = "icons/yellow.png";
+    greenghost = new Image();
+    greenghost.src = "icons/green.png";
 
 }
